2f13d0031b8cc6fcd5db40a9b2bb0857d85c1ac2
Rawakinode <aliceasua94@gmail.com>
2023-11-21 17:59:33 +0800

    Pesan commit Anda di sini

---
+++ src/pages/index.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+import {
+  useDashboard,
+  LoadingStatus,
+  type ChainConfig,
+} from '@/stores/useDashboard';
+import ChainSummary from '@/components/ChainSummary.vue';
+import { computed, ref } from 'vue';
+import { useBlockchain } from '@/stores';
+
+const dashboard = useDashboard();
+
+const keywords = ref('');
+const chains = computed(() => {
+  if (keywords.value) {
+    return Object.values(dashboard.chains).filter(
+      (x: ChainConfig) => x.chainName.indexOf(keywords.value) > -1
+    );
+  } else {
+    return Object.values(dashboard.chains);
+  }
+});
+const chainStore = useBlockchain()
+</script>
+<template>
+  <div class="">
+    <div class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2">
+      <div class="w-16 rounded-full">
+        
+      </div>
+      <h1 class="text-primary dark:invert text-3xl md:!text-6xl font-bold" style="color: brown;">
+        {{ $t('pages.title') }}
+      </h1>
+      <div class="badge badge-primary badge-outline dark:invert mt-1 text-sm md:!mt-8" style="color: brown;">
+        {{ $t('pages.tag') }}
+      </div>
+    </div>
+    <div class="text-center text-base">
+      <p class="mb-1">
+        {{ $t('pages.slogan') }}
+      </p>
+      <h2 class="mb-6">{{ $t('pages.description') }}</h2>
+    </div>
+    <div
+      v-if="dashboard.status !== LoadingStatus.Loaded"
+      class="flex justify-center"
+    >
+      <progress class="progress progress-info w-80 h-1"></progress>
+    </div>
+
+    <div class="flex items-center rounded-lg bg-base-100  border border-gray-200 dark:border-gray-700 mt-10">
+      <Icon icon="mdi:magnify" class="text-2xl text-gray-400 ml-3"/>
+      <input :placeholder="$t('pages.search_placeholder')" class="px-4 h-10 bg-transparent flex-1 outline-none text-base" v-model="keywords" />
+      <div class="px-4 text-base hidden md:!block">{{ chains.length }}/{{ dashboard.length }}</div>
+    </div>
+
+    <div
+      class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5"
+    >
+      <ChainSummary
+        v-for="(chain, index) in chains"
+        :key="index"
+        :name="chain.chainName"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+ .logo path{
+  fill: #171d30;
+}
+</style>
